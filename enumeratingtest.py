892180215646d71cd88b9bb6369a7adb6f8626c8
Razmirus <44821779+Razmirus@users.noreply.github.com>
2024-10-31 09:36:36 +0100

    Update enumeratingtest.py
    add returnvalue instead of printing

--- enumeratingtest.py
+++ enumeratingtest.py
@@ -1,30 +1,30 @@
 import sys
 
 def backupnums(currnum):
-    
+    returnval = []
     for i in range(10):
         while(currnum % pow(2,i) != 0):
             currnum = currnum -1
         for _ in range(3):
-            print(currnum,end="; ")
+            returnval.append(currnum)
             currnum = currnum - pow(2,i)
             if currnum < pow(2,i) :
                 break
         if currnum < pow(2,i)+1 :
             break
-    print()        
+    return tuple(returnval)        
    
 def main():
     num = 30
     args = sys.argv[1:]
     if len(args) >= 1:
         num = int(args[0])
-    for i in range(30):
-        print(i+1,end="; ")
-    print()
+    vals = tuple(range(1,31))
+    print(vals)   
 
     for i in range(num):
-        backupnums(i+1)
+        vals = backupnums(i+1)
+        print(vals)
 
 if __name__ == "__main__":
     main()
