0d47fd4963843b6b65598cbc302383881a1795f0
Razmirus <44821779+Razmirus@users.noreply.github.com>
2024-10-31 07:50:17 +0100

    Update enumeratingtest.py
    Can pass the number as argument, default is 30

--- enumeratingtest.py
+++ enumeratingtest.py
@@ -1,9 +1,11 @@
+import sys
+
 def backupnums(currnum):
     
     for i in range(10):
         while(currnum % pow(2,i) != 0):
             currnum = currnum -1
-        for j in range(3):
+        for _ in range(3):
             print(currnum,end="; ")
             currnum = currnum - pow(2,i)
             if currnum < pow(2,i) :
@@ -13,11 +15,15 @@ def backupnums(currnum):
     print()        
    
 def main():
+    num = 30
+    args = sys.argv[1:]
+    if len(args) >= 1:
+        num = int(args[0])
     for i in range(30):
         print(i+1,end="; ")
     print()
 
-    for i in range(30):
+    for i in range(num):
         backupnums(i+1)
 
 if __name__ == "__main__":
