c9f803170f11efbed2d216f7e6ad8fa901a62a49
Razmirus <44821779+Razmirus@users.noreply.github.com>
2024-11-01 12:56:02 +0100

    Update makesnap.sh
    Description how to add this to crontab

--- makesnap.sh
+++ makesnap.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
+
+# add this script to crontab using:
+# sudo crontab -e
+# for example this will run the script at 12:43 every day:
+# 43  12 *   *   *     /path_to_script/makesnap.sh
+
 btrfs subvolume snapshot -r /mountpoint/ /mountpoint/.snapshots/@GMT_`date +%Y.%m.%d-%H.%M.%S`
